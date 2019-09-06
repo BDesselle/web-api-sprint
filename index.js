a7dc434b5611eb8bd5181f4eb046850df7fe318d
Brandon Desselle <desselle.brandon2@gmail.com>
2019-09-06 16:42:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 require("dotenv").config();
 const server = require("./server");
 const port = process.env.PORT || 6000;
-/* const ora = require("ora");
-const loader = ora(); */
 
 server.listen(port, () => console.log(`Server running on port ${port}...`));
